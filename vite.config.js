19465a7ea7c546715d155c73f6597bdd1df1d3e6
KETEMAASIRES <ketemaasires@gmail.com>
2025-07-06 12:46:40 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix/",
-});
-
-// // vite.config.js
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//   base: '/your-repo-name/', // 👈 use your actual GitHub repo name here
-// });
+  base: '/Netflix/', // 👈 IMPORTANT: Set this to match your repo name
+})
